4e9d6cd9b013bcbc018e0dc0d6a433e8f3501b43
Michael Calkins <m.calkins576@gmail.com>
2019-11-08 18:06:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :name
+
+  @@roster = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_student(student, grade)
+        @@roster[grade] = [] unless @@roster.has_key?(grade)
+        @@roster[grade] << student
+        
+  end
+  
+  def roster
+    @@roster 
+  end
+  
+  def grade(grade)
+    @@roster[grade]
+  end
+  
+  def sort
+    sorted_roster = Hash[@@roster.sort_by{|grade, students| grade}]
+  end
+   
+end
