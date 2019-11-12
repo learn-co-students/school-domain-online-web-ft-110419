816bc76fe48de903089b5223023a1c3f04d97976
Jess F <robotspacefish@gmail.com>
2019-11-12 01:33:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] = [] if !roster[grade]
+    roster[grade] << student_name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.collect do |grade, students|
+      # students.sort!
+      # not using bang because I don't want to permanently affect the roster
+      roster[grade] = students.sort
+    end
+    roster
+  end
+end
