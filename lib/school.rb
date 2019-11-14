e1eedc057d76aaa1a9859808cc312afd130be4fb
Ken Goad <goadke@g.cofc.edu>
2019-11-14 03:12:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @name = name
+  if @roster[grade]
+     @roster[grade] << @name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+ @roster.each {|key, value| value.sort!}
+end
+end 
